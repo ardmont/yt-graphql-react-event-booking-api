e3d1071152a03c7fd7d676a66b2503af97f0a23c
Ardiles <ardiles.montenegro@gmail.com>
2019-04-09 16:31:49 -0300

    Standardizes code with standardjs.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', (req, res, next) => {
-    res.send('Hello World!');
+  res.send('Hello World!')
 })
 
-app.listen(3000);
+app.listen(3000)
