ea779382e1513dc6475c9a62ce4d699611c9ec20
Ardiles <ardiles.montenegro@gmail.com>
2019-04-13 21:51:20 -0300

    5-GraphQL + MongoDB

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+
+const Event = require('./models/event')
 
 const app = express()
-
-const events = []
 
 app.use(bodyParser.json())
 
@@ -41,18 +42,37 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return events
+      return Event.find()
+        .then(events => {
+          return events
+        })
+        .catch(err => {
+          throw err
+        })
     },
     createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+      // const event = {
+      //   _id: Math.random().toString(),
+      //   title: args.eventInput.title,
+      //   description: args.eventInput.description,
+      //   price: +args.eventInput.price,
+      //   date: args.eventInput.date
+      // }
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: args.eventInput.date
-      }
-      events.push(event)
-      return event
+        date: new Date(args.eventInput.date)
+      })
+      return event.save()
+        .then(result => {
+          console.log(result)
+          return { ...result._doc }
+        })
+        .catch(err => {
+          console.log(err)
+          throw err
+        })
     }
   },
   graphiql: true
@@ -62,4 +82,6 @@ app.get('/', (req, res, next) => {
   res.send('Hello World!')
 })
 
-app.listen(3000)
+mongoose.connect(process.env.MONGODBURL, { useNewUrlParser: true })
+  .then(app.listen(3000))
+  .catch((err) => { console.log(err) })
