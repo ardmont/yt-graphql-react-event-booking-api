bda6c00a54eb9b7f2f7448355c17e9031ad4f253
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-05-18 03:06:57 -0300

    13 Hitting the API  -  Initiates SubmitHandler.

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -3,19 +3,39 @@ import React, { Component } from 'react'
 import './Auth.css'
 
 class AuthPage extends Component {
+  constructor (props) {
+    super(props)
+    this.emailEl = React.createRef()
+    this.passwordEl = React.createRef()
+
+    this.submitHandler = this.submitHandler.bind(this)
+  }
+
+  submitHandler (event) {
+    event.preventDefault()
+    const email = this.emailEl.current.value
+    const password = this.passwordEl.current.value
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return
+    }
+
+    console.log(email + ' ' + password)
+  }
+
   render () {
-    return <form className='auth-form'>
+    return <form className='auth-form' onSubmit={this.submitHandler}>
       <div className='form-control'>
         <label htmlFor='email'>Email</label>
-        <input type='email' id='email' />
+        <input type='email' id='email' ref={this.emailEl} />
       </div>
       <div className='form-control'>
         <label htmlFor='password'>Password</label>
-        <input type='password' id='password' />
+        <input type='password' id='password' ref={this.passwordEl} />
       </div>
       <div className='form-actions'>
-        <button type='button'>Switch to Signup</button>
         <button type='submit'>Submit</button>
+        <button type='button'>Switch to Signup</button>
       </div>
     </form>
   }
